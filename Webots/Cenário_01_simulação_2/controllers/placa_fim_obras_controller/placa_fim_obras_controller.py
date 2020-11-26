3e0391ab3721cdfef2197ffbb1a996eb582efb6a
TCCFEI2020 <75094383+TCCFEI2020@users.noreply.github.com>
2020-11-26 17:21:49 -0300

    upload da simulação 2
    Adicionado a simulação 2 na pasta Webots

---
+++ Webots/Cenário_01_simulação_2/controllers/placa_fim_obras_controller/placa_fim_obras_controller.py
@@ -0,0 +1,16 @@
+"""Controlador do sistema de RF acoplado a placa."""
+from controller import Robot
+from controller import Emitter
+
+# create the Robot instance.
+robot = Robot()
+# get the time step of the current world.
+
+timestep = int(robot.getBasicTimeStep())
+# em=robot.getDeviceByIndex(0)
+emitter = Emitter("emitter")
+
+# Transmite a mensagem RF
+while robot.step(timestep) != -1:
+
+    emitter.send('PXBBBB080'.encode('utf-8'))
---
+++ Webots/Cenário_01_simulação_2/controllers/placa_sentido_contario_controller/placa_sentido_contario_controller.py
@@ -0,0 +1,17 @@
+"""Controlador do sistema de RF acoplado a placa."""
+from controller import Robot
+from controller import Emitter
+
+# create the Robot instance.
+robot = Robot()
+# get the time step of the current world.
+
+timestep = int(robot.getBasicTimeStep())
+# em=robot.getDeviceByIndex(0)
+
+emitter = Emitter("emitter")
+
+# Transmite a mensagem RF
+while robot.step(timestep) != -1:
+
+    emitter.send('NXBBBB060'.encode('utf-8'))
